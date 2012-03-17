8e781d5c1dc6b8db58d17ea9b233aef88faa3755
toxicate20 <alex.runde@gmail.com>
2012-03-17 14:59:39 +0100

    new tests and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 SampleApp::Application.routes.draw do
+  
+  root :to => "pages#home"
+  
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
+
   get "pages/home"
   get "pages/contact"
   get "pages/about"
+  get "pages/help"
+  
+
 end
