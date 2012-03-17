f3ec9cddf0b79adb938f0bfcdf37fccd107f98bf
toxicate20 <alex.runde@gmail.com>
2012-03-17 19:27:33 +0100

    new password hash

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -98,6 +98,46 @@ describe User do
       @user.should respond_to(:encrypted_password)
     end
     
+    it "should set the encrypted password attributed" do
+      @user.encrypted_password.should_not be_blank
+    end
+    
+    it "should have a salt" do
+      @user.should respond_to(:salt)
+    end
+    
+    describe "has_password? method" do
+      it "should exist" do
+        @user.should respond_to(:has_password?)
+      end
+      
+      it "should return true if the passwords match" do
+        @user.has_password?(@attr[:password]).should be_true
+      end
+      
+      it "should return false if the passwords dont match" do
+        @user.has_password?("invalid").should be_false        
+      end
+    end
+    
+    describe "authenticate method" do
+      it "should return nil on email/password mismatch" do
+        User.authenticate(@attr[:email], "wrongpass").should be_nil
+      end
+      
+      it "should return nil for an emial adress with no user" do
+        User.authenticate("bar@foo.com", @attr[:password]).should be_nil
+      end
+      
+      it "should return the user on email/password match" do
+        User.authenticate(@attr[:email], @attr[:password]).should == @user
+      end
+      
+      it "should exist" do
+        User.should respond_to(:authenticate)
+      end
+      
+    end
   
   end
 
