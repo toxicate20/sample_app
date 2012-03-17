d3c738ed57a4ba225e5577bae07d9370a9ab3dc6
toxicate20 <alex.runde@gmail.com>
2012-03-17 15:35:24 +0100

    added user controller and tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 SampleApp::Application.routes.draw do
-  
   root :to => "pages#home"
   
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
+  match '/signup', :to => 'users#new'
 
   get "pages/home"
   get "pages/contact"
   get "pages/about"
   get "pages/help"
+  get "users/new"
   
 
 end
